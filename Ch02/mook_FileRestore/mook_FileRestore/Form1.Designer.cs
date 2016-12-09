6c2dfe14f17e01dc5bb5e879c3f8fb97e61d79d2
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2016-12-09 09:12:25 +0900

    mook_FileRestore 프로젝트 시작

---
+++ Ch02/mook_FileRestore/mook_FileRestore/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace mook_FileRestore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvRcvFile = new System.Windows.Forms.ListView();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvRcvFile
+            // 
+            this.lvRcvFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chPath,
+            this.chDel});
+            this.lvRcvFile.FullRowSelect = true;
+            this.lvRcvFile.GridLines = true;
+            this.lvRcvFile.Location = new System.Drawing.Point(12, 12);
+            this.lvRcvFile.Name = "lvRcvFile";
+            this.lvRcvFile.Size = new System.Drawing.Size(569, 420);
+            this.lvRcvFile.TabIndex = 0;
+            this.lvRcvFile.UseCompatibleStateImageBehavior = false;
+            this.lvRcvFile.View = System.Windows.Forms.View.Details;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(277, 439);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(94, 23);
+            this.btnDel.TabIndex = 1;
+            this.btnDel.Text = "휴지통 비우기";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(377, 439);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(94, 23);
+            this.btnRestore.TabIndex = 1;
+            this.btnRestore.Text = "복원";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(477, 439);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "새로고침";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 150;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "원래 위치";
+            this.chPath.Width = 260;
+            // 
+            // chDel
+            // 
+            this.chDel.Text = "삭제된 날짜";
+            this.chDel.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 469);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.lvRcvFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "휴지통 복원";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvRcvFile;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chPath;
+        private System.Windows.Forms.ColumnHeader chDel;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Ch02/mook_FileRestore/mook_FileRestore/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[휴지통 복원]
+ * 프로그램으로 휴지통에 담겨 있는 파일에 접근하기 위해서는 닷넷의 파일클래스로는 권한문제로 어렵다
+ * win32 API를 활용하여 특정 폴더 및 파일에 접근이 가능하다
+
+#이벤트 핸들러
+ - Form1_Load(object sender, EventArgs e) : 휴지통에 담겨 있는 파일 리스트 출력
+ - btnRefresh_Click(object sender, EventArgs e) : [새로고침] 버튼 이벤트 핸들러, 휴지통에 담겨있는 파일 리스11트 출력
+ - btnDel_Click(object sender, EventArgs e) : [휴지통 비우기] 버튼 이벤트 핸들러, 휴지통에 담겨 있는 파일 삭제
+ - btnRestore_Click(object sender, EventArgs e) : [복원] 버튼 이벤트 핸들러, 휴지통 파일 중 선택된 파일 복원
+
+#참조(WIN32 API)
+ - shell32.dll : c:\Windows\System32\ 경로에서 shell32.dll 파일을 추가
+*/
+namespace mook_FileRestore
+{
+    public partial class Form1 : Form
+    {
+        //SHEmptyRecycleBin의 세번째 매개변수 값을 설정하는 작업 수행
+        enum RecycleFlags : uint
+        {
+            SHERB_NOCONFIRMATION = 0x00000001,
+            SHERB_NOPROGRESSUI = 0x00000002,
+            SHERB_NOSOUND = 0x0000004
+        }
+
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+        //SHEmptyRecycleBin : 시스템의 휴지통을 비우는 작업 수행
+        //첫번째 인수 : Process의 Handle을 전달
+        //세번째 인수 : 0x00000001(관련 메세지가 표현되지 않도록)
+        //             0x00000002(휴지통에 대량의 데이터가 존재할 경우 휴지통을 비우는 진행상태가 표시되지 않도록
+        //             0x00000004(휴지통을 비울때 소리나지 않도록
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
