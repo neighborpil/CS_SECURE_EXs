b9c11ea8be82e5e687289acf25f354c80c5ee92c
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2016-12-15 17:04:42 +0900

    mook_UTF8Base64 생성 및 코드 수정

---
+++ Ch03/mook_UTF8Base64/mook_UTF8Base64/Conv.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mook_UTF8Base64
+{
+    class Conv
+    {
+        public string sTemp1 = null, sTemp2 = null;
+
+        //문자열을 파라미터로 전달받아 UTF-8 형식으로 인코딩
+        public string UTF8Encode(string sParam)
+        {
+            sTemp1 = sParam;
+            sTemp2 = string.Empty; //빈 문자열을 설정, ""와 같다. 결국 초기화 하는 구문
+
+            if(sParam == string.Empty || sParam == null) //매개변수로 받은 값이 ""의 빈문자열이거나 null이면
+            {
+                sTemp2 = "입력된 문자가 없습니다.";
+                return sTemp2;
+            }
+
+            UTF8Encoding _UTF8 = new UTF8Encoding(); //UTF8 Encoding 클래스의 개체를 초기화하는 구문, 유니코드 문자를 UTF-8인코딩으로 변환
+
+            try
+            {
+                byte[] enbytes = _UTF8.GetBytes(sTemp1); //byte배열을 초기화, 지정된 문자열의 모든 문자를 바이트로 인코딩
+
+                #region foreach 구문을 이용하여 13행에서 바이트로 인코딩된 배열의 컬렉션 값을 가져오는 작업 수행
+                foreach (byte bt in enbytes) //byte단위 문자와 매칭되는 16진수 값을 sTemp2에 저장
+                {
+                    sTemp2 += string.Format("%{0:X2}", bt); //UTF-8 형식으로 변환하기 위해 '%'를 앞에 붙여준다
+                }                                           //{0:X2} 표현은 문자를 2자의 16진수 Hex값으로 변환하는 구문
+                #endregion
+                sTemp2 = sTemp2.ToUpper();
+            }
+            catch(Exception ex)
+            {
+                sTemp2 = "오류발생 : " + ex.Message.ToString();
+            }
+            return sTemp2; //변환된 문자열을 반환
+        }
+        //UTF-8로 변환된 문자열을 본래의 문자열로 디코딩
+        public string UTF8Decode(string sParam)
+        {
+            sTemp1 = sParam;
+            sTemp2 = string.Empty;
+            int discarded;
+
+            if(sParam == string.Empty || sParam == null)
+            {
+                sTemp2 = "입력된 문자가 없습니다";
+                return sTemp2;
+            }
+
+            try
+            {
+                byte[] debytes = GetBytes(sTemp1, out discarded); //utf-8로 인코딩된 문자값을 16진수 바이트 배열로 반환
+                char[] chars = new char[debytes.Length];
+                chars = Encoding.UTF8.GetChars(debytes);
+
+                foreach(char c in chars) //char배열에 저장된 문자 컬렉션을 가져와 sTemp2에 저장
+                {
+                    sTemp2 += c;
+                }
+            }
+            catch(Exception ex)
+            {
+                sTemp2 = "오류발생 : " + ex.Message.ToString();
+            }
+            return sTemp2;
+
+        }
+
+        //UTF-8로 인코딩된 문자 값을 파라미터로 전달받아 16진수로 구성된 바이트 배열을 반환하는 작업 수행
+        public static byte[] GetBytes(string hexString, out int discarded)
+        {
+            discarded = 0;
+            string newString = "";
+            char c;
+            for (int i = 0; i < hexString.Length; i++) //입력받은 문자열의 길이만큼
+            {
+                c = hexString[i]; //하나씩 c에 집어넣어서
+
+                if (IsHexDigit(c)) //16진수로 표현되는 값이면 newString에 추가하고
+                    newString += c;
+                else               //아니면 버리는 값 discarded에 +1
+                    discarded++;   //'%'값을 지우는 역할을 한다
+            }
+            if(newString.Length % 2 != 0) //만약 매개변수 string이 홀수라면 
+            {
+                discarded++; //버리는 값이 1 추가하고
+                newString = newString.Substring(0, newString.Length - 1); //마지막 하나를 자른다
+            }
+
+            #region 16진수 문자열을 분리하여 두 자리씩 hex에 저장하여 HexToByte() 메서드를 호출하여 16진수를 10진수 값으로 변환하여 byte배열에 저장
+            int byteLength = newString.Length / 2; //newString을 반으로 잘라
+            byte[] bytes = new byte[byteLength]; //반으로 자른 길이만큼 byte 배열을 만들고
+            string hex;
+            int j = 0;
+            
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hex = new string(new char[] { newString[j], newString[j + 1] }); //2개씩 붙여서 16진수 값 string으로 만든다
+                bytes[i] = HexToByte(hex);
+                j = j + 2;
+            } 
+            #endregion
+            return bytes;
+        }
+
+        //bool 타입을 반환하는 메소드, 파라미터 값으로 전달 받은 문자가 16진수로 유효한지 검사하는 작업
+        public static bool IsHexDigit(char c)
+        {
+            int numChar;
+            int numA = Convert.ToInt32('A');
+            int num1 = Convert.ToInt32('0');
+            c = char.ToUpper(c);
+            numChar = Convert.ToInt32(c);
+            if (numChar >= numA && numChar < (numA + 6)) //입력받은 char이 A ~ F이면 true 반환
+                return true;
+            if (numChar >= num1 && numChar < (num1 + 10)) //입력받은 char이 0 ~ 9이면 true 반환
+                return true;
+            return false; //아니면 false 반환
+        }
+
+        //전달받은 파라미터 값을 10진수로 변환하여 반환
+        private static byte HexToByte(string hex)
+        {
+            if (hex.Length > 2 || hex.Length <= 0)
+                throw new ArgumentException("hex mus be 1 or 2 characters in length");
+            byte newByte = byte.Parse(hex, System.Globalization.NumberStyles.HexNumber); //16진수를 10진수로 변환하여 byte변수에 저장
+            return newByte;
+        }
+
+        //문자열을 파라미터로 전달받아 Base64형식으로 인코딩
+    }
+}
---
+++ Ch03/mook_UTF8Base64/mook_UTF8Base64/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace mook_UTF8Base64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUTFEn = new System.Windows.Forms.Button();
+            this.btnUTFDe = new System.Windows.Forms.Button();
+            this.btnBaseEn = new System.Windows.Forms.Button();
+            this.btnBaseDe = new System.Windows.Forms.Button();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUTFEn
+            // 
+            this.btnUTFEn.Location = new System.Drawing.Point(13, 12);
+            this.btnUTFEn.Name = "btnUTFEn";
+            this.btnUTFEn.Size = new System.Drawing.Size(108, 23);
+            this.btnUTFEn.TabIndex = 0;
+            this.btnUTFEn.Text = "UTF8 Encode";
+            this.btnUTFEn.UseVisualStyleBackColor = true;
+            this.btnUTFEn.Click += new System.EventHandler(this.btnUTFEn_Click);
+            // 
+            // btnUTFDe
+            // 
+            this.btnUTFDe.Location = new System.Drawing.Point(13, 41);
+            this.btnUTFDe.Name = "btnUTFDe";
+            this.btnUTFDe.Size = new System.Drawing.Size(108, 23);
+            this.btnUTFDe.TabIndex = 0;
+            this.btnUTFDe.Text = "UTF8 Decode";
+            this.btnUTFDe.UseVisualStyleBackColor = true;
+            this.btnUTFDe.Click += new System.EventHandler(this.btnUTFDe_Click);
+            // 
+            // btnBaseEn
+            // 
+            this.btnBaseEn.Location = new System.Drawing.Point(13, 119);
+            this.btnBaseEn.Name = "btnBaseEn";
+            this.btnBaseEn.Size = new System.Drawing.Size(108, 23);
+            this.btnBaseEn.TabIndex = 0;
+            this.btnBaseEn.Text = "Base64 Encode";
+            this.btnBaseEn.UseVisualStyleBackColor = true;
+            // 
+            // btnBaseDe
+            // 
+            this.btnBaseDe.Location = new System.Drawing.Point(13, 148);
+            this.btnBaseDe.Name = "btnBaseDe";
+            this.btnBaseDe.Size = new System.Drawing.Size(108, 23);
+            this.btnBaseDe.TabIndex = 0;
+            this.btnBaseDe.Text = "Base64 Decode";
+            this.btnBaseDe.UseVisualStyleBackColor = true;
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Location = new System.Drawing.Point(127, 12);
+            this.txtEncode.Multiline = true;
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(354, 99);
+            this.txtEncode.TabIndex = 1;
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Location = new System.Drawing.Point(127, 119);
+            this.txtDecode.Multiline = true;
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(354, 99);
+            this.txtDecode.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 226);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.txtEncode);
+            this.Controls.Add(this.btnBaseDe);
+            this.Controls.Add(this.btnBaseEn);
+            this.Controls.Add(this.btnUTFDe);
+            this.Controls.Add(this.btnUTFEn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "UTF / Base64 EnDecoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUTFEn;
+        private System.Windows.Forms.Button btnUTFDe;
+        private System.Windows.Forms.Button btnBaseEn;
+        private System.Windows.Forms.Button btnBaseDe;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.TextBox txtDecode;
+    }
+}
+
