5c7af2fa6a12b6bcb273ca70553149dab8b51fef
neighborpil <neighborpil@naver.com>
2016-12-20 00:13:47 +0900

    cd

--- Ch03/mook_cdkey/mook_cdkey/Form1.Designer.cs
+++ Ch03/mook_cdkey/mook_cdkey/Form1.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDateYear = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtMacAdd = new System.Windows.Forms.TextBox();
+            this.txtCKey = new System.Windows.Forms.TextBox();
+            this.btnKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key Year :";
+            // 
+            // cbDateYear
+            // 
+            this.cbDateYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDateYear.FormattingEnabled = true;
+            this.cbDateYear.Items.AddRange(new object[] {
+            "2015",
+            "2016",
+            "2017",
+            "2018"});
+            this.cbDateYear.Location = new System.Drawing.Point(110, 10);
+            this.cbDateYear.Name = "cbDateYear";
+            this.cbDateYear.Size = new System.Drawing.Size(148, 20);
+            this.cbDateYear.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mac Add :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "CD-Key :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(110, 38);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(222, 21);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtMacAdd
+            // 
+            this.txtMacAdd.Location = new System.Drawing.Point(110, 69);
+            this.txtMacAdd.Name = "txtMacAdd";
+            this.txtMacAdd.Size = new System.Drawing.Size(222, 21);
+            this.txtMacAdd.TabIndex = 2;
+            // 
+            // txtCKey
+            // 
+            this.txtCKey.Location = new System.Drawing.Point(110, 99);
+            this.txtCKey.Name = "txtCKey";
+            this.txtCKey.ReadOnly = true;
+            this.txtCKey.Size = new System.Drawing.Size(351, 21);
+            this.txtCKey.TabIndex = 2;
+            // 
+            // btnKey
+            // 
+            this.btnKey.Location = new System.Drawing.Point(340, 8);
+            this.btnKey.Name = "btnKey";
+            this.btnKey.Size = new System.Drawing.Size(121, 82);
+            this.btnKey.TabIndex = 3;
+            this.btnKey.Text = "CD-Key 생성";
+            this.btnKey.UseVisualStyleBackColor = true;
+            this.btnKey.Click += new System.EventHandler(this.btnKey_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(481, 130);
+            this.Controls.Add(this.btnKey);
+            this.Controls.Add(this.txtCKey);
+            this.Controls.Add(this.txtMacAdd);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.cbDateYear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CD키 생성";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDateYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtMacAdd;
+        private System.Windows.Forms.TextBox txtCKey;
+        private System.Windows.Forms.Button btnKey;
     }
 }
 
--- Ch03/mook_cdkey/mook_cdkey/Form1.cs
+++ Ch03/mook_cdkey/mook_cdkey/Form1.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+/*
+[CD-Key 만들기]
+ - User name, 사용연도, MAC값을 혼용하여 25자리의 CD-Key를 생성하는 애플리케이션
+ - CD-Key를 생성하는 'mook_Kegen' 프로젝트와
+ - 로컬 컴퓨터에서 CD-Key 인증을 수행하는 'mook_cdkey' 애플리케이션으로 구성
+ - CD-Key를 직접 배포하는 방식으로 구현
 
+#이벤트 핸들러
+ - Form1_Load(object sender, EventArgs e) : 폼 로딩 이벤트 핸들러, cdDateYear 컨트롤 입력과 MAC 값을 입력
+ - btnKey_Click(object sender, EventArgs e) : [CD Key 생성] 버튼 이벤트 핸들러, CD-Key 생성
+
+※ 'System.Management' 어셈블리를 [참조 추가] 한다
+※ cbDateYear의 컬렉션 값은 2015, 2016, 2017, 2018이다
+*/
 namespace mook_cdkey
 {
     public partial class Form1 : Form
     {
+        //멤버 변수
+        //CD-Key 생성에 쓰일 배열멤버변수, 사용연도, User Name, MAC 값으로 얻은 첨자 값에 매칭되는 값을 CD-Key로 사용
+        private char[] K = new char[]
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //폼이 실행될 때 MAC 주소값을 얻는 작업 수행
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cbDateYear.Text = DateTime.Now.Year.ToString();
+            ObjectQuery oq = new ObjectQuery("SELECT * FROM Win32_NetworkAdapter"); //MAC 주소를 구하기 위한 쿼리 문자열 설정
+            ManagementObjectSearcher query = new ManagementObjectSearcher(oq); //컴퓨터 관리 정보에 대한 지정된 쿼리 호출
+            foreach(ManagementObject mo in query.Get()) //관리정보 컬렉션 값을 이용하여 MAC주소 구하기
+            {
+                if (mo["MACAddress"] != null)
+                    this.txtMacAdd.Text = mo["MACAddress"].ToString().Replace(":", "");
+            }
+        }
+
+        //사용연도, MAC 주소, User Name값을 이용하여 CD-Key를 구하는 작업 수행
+        private void btnKey_Click(object sender, EventArgs e)
+        {
+            if(this.txtUserName.Text == "")
+            {
+                MessageBox.Show("사용자 이름을 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtUserName.Focus();
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(K[Convert.ToInt32(this.cbDateYear.Text) % 36]);
+            sb.Append(K[Convert.ToInt32(this.cbDateYear.Text.Substring(0, 1))]);
+            sb.Append(K[Convert.ToInt32(this.cbDateYear.Text.Substring(2, 1))]);
+            sb.Append(K[Convert.ToInt32(this.cbDateYear.Text.Substring(3, 1))]);
+            sb.Append(this.txtMacAdd.Text);
+            sb.Append(this.txtMacAdd.Text.Substring(0, 1));
+            sb.Append(this.txtMacAdd.Text.Substring(1, 1));
+            sb.Append(MD5Hash(this.txtUserName.Text).Substring(0, 1));
+            sb.Append(MD5Hash(this.txtUserName.Text).Substring(29, 1));
+
+            StringBuilder sbN = new StringBuilder();
+
+            string cd = sb.ToString().ToUpper();
+            char[] key = cd.ToCharArray();
+            int[] num = new int[key.Length];
+            int add = DateTime.Now.Year;
+            int z;
+            int con0 = 0;
+            int con1 = 0;
+            int con2 = 0;
+            int con3 = 0;
+            int con4 = 0;
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                z = (i + 10) * add;
+                num[i] = (int)key[i];
+
+            }
         }
     }
 }
