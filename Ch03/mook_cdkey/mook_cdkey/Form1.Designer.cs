40b68b86398d1aad283e94420f75177126f78685
neighborpil <neighborpil@naver.com>
2016-12-23 20:47:35 +0900

    cdkey make 완성

--- Ch03/mook_cdkey/mook_cdkey/Form1.Designer.cs
+++ Ch03/mook_cdkey/mook_cdkey/Form1.Designer.cs
@@ -42,9 +42,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(15, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.Size = new System.Drawing.Size(74, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Key Year :";
             // 
@@ -57,65 +57,71 @@
             "2016",
             "2017",
             "2018"});
-            this.cbDateYear.Location = new System.Drawing.Point(110, 10);
+            this.cbDateYear.Location = new System.Drawing.Point(126, 12);
+            this.cbDateYear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbDateYear.Name = "cbDateYear";
-            this.cbDateYear.Size = new System.Drawing.Size(148, 20);
+            this.cbDateYear.Size = new System.Drawing.Size(169, 23);
             this.cbDateYear.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Location = new System.Drawing.Point(15, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.Size = new System.Drawing.Size(88, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "User Name :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Location = new System.Drawing.Point(15, 90);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 12);
+            this.label3.Size = new System.Drawing.Size(75, 15);
             this.label3.TabIndex = 0;
             this.label3.Text = "Mac Add :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 102);
+            this.label4.Location = new System.Drawing.Point(15, 128);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 12);
+            this.label4.Size = new System.Drawing.Size(69, 15);
             this.label4.TabIndex = 0;
             this.label4.Text = "CD-Key :";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(110, 38);
+            this.txtUserName.Location = new System.Drawing.Point(126, 48);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(222, 21);
+            this.txtUserName.Size = new System.Drawing.Size(253, 25);
             this.txtUserName.TabIndex = 2;
             // 
             // txtMacAdd
             // 
-            this.txtMacAdd.Location = new System.Drawing.Point(110, 69);
+            this.txtMacAdd.Location = new System.Drawing.Point(126, 86);
+            this.txtMacAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtMacAdd.Name = "txtMacAdd";
-            this.txtMacAdd.Size = new System.Drawing.Size(222, 21);
+            this.txtMacAdd.ReadOnly = true;
+            this.txtMacAdd.Size = new System.Drawing.Size(253, 25);
             this.txtMacAdd.TabIndex = 2;
             // 
             // txtCKey
             // 
-            this.txtCKey.Location = new System.Drawing.Point(110, 99);
+            this.txtCKey.Location = new System.Drawing.Point(126, 124);
+            this.txtCKey.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtCKey.Name = "txtCKey";
             this.txtCKey.ReadOnly = true;
-            this.txtCKey.Size = new System.Drawing.Size(351, 21);
+            this.txtCKey.Size = new System.Drawing.Size(401, 25);
             this.txtCKey.TabIndex = 2;
             // 
             // btnKey
             // 
-            this.btnKey.Location = new System.Drawing.Point(340, 8);
+            this.btnKey.Location = new System.Drawing.Point(389, 10);
+            this.btnKey.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnKey.Name = "btnKey";
-            this.btnKey.Size = new System.Drawing.Size(121, 82);
+            this.btnKey.Size = new System.Drawing.Size(138, 102);
             this.btnKey.TabIndex = 3;
             this.btnKey.Text = "CD-Key 생성";
             this.btnKey.UseVisualStyleBackColor = true;
@@ -123,9 +129,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 130);
+            this.ClientSize = new System.Drawing.Size(550, 162);
             this.Controls.Add(this.btnKey);
             this.Controls.Add(this.txtCKey);
             this.Controls.Add(this.txtMacAdd);
@@ -136,6 +142,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "CD키 생성";
