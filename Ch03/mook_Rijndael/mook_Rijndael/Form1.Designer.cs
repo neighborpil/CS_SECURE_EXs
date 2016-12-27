3926bc8fe0658421e98adbbce4c4102e3d583abc
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2016-12-27 09:06:20 +0900

    Rijndael 암복호화 시작

---
+++ Ch03/mook_Rijndael/mook_Rijndael/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace mook_Rijndael
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtDiary = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(21, 12);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "File Open";
+            this.btnFile.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Private Key :";
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Location = new System.Drawing.Point(194, 12);
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.Size = new System.Drawing.Size(224, 21);
+            this.txtPrivateKey.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(424, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Decrypt";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(505, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "File Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtDiary
+            // 
+            this.txtDiary.Location = new System.Drawing.Point(21, 41);
+            this.txtDiary.Multiline = true;
+            this.txtDiary.Name = "txtDiary";
+            this.txtDiary.Size = new System.Drawing.Size(559, 293);
+            this.txtDiary.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 349);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtDiary);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rijndael Diary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtDiary;
+    }
+}
+
---
+++ Ch03/mook_Rijndael/mook_Rijndael/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[Rijndael 암&복호화]
+ - AES 후보 기술
+ - 3가지 키 크기(128bit, 192bit, 256bit) 지원
+ - 새로운 대칭 블록 암호
+ - 서로 다른 다양한 컴퓨터 환경에서도 우수한 성능
+ - 메모리 적게 차지해 스마트카드 등 메모리 용량이 작은 장치에서도 손쉽게 쓸 수 있는 강력한 암&복호화 알고리즘
+*/
+namespace mook_Rijndael
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
