f505e1117f437a4913dfe5be854b25a1ad9300e4
neighborpil <neighborpil@naver.com>
2017-01-07 11:18:45 +0900

    RSA User making

---
+++ Ch03/mook_RSAUser/mook_RSAUser/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace mook_RSAUser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.publicKeyLoadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.msMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenu
+            // 
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(619, 24);
+            this.msMenu.TabIndex = 0;
+            this.msMenu.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.publicKeyLoadToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitEToolStripMenuItem});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일FToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // publicKeyLoadToolStripMenuItem
+            // 
+            this.publicKeyLoadToolStripMenuItem.Name = "publicKeyLoadToolStripMenuItem";
+            this.publicKeyLoadToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.publicKeyLoadToolStripMenuItem.Text = "Public Key Load";
+            this.publicKeyLoadToolStripMenuItem.Click += new System.EventHandler(this.publicKeyLoadToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(157, 6);
+            // 
+            // exitEToolStripMenuItem
+            // 
+            this.exitEToolStripMenuItem.Name = "exitEToolStripMenuItem";
+            this.exitEToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.exitEToolStripMenuItem.Text = "Exit(&X)";
+            this.exitEToolStripMenuItem.Click += new System.EventHandler(this.exitEToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMessage);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(517, 124);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "메시지";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(6, 15);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(505, 103);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtEncrypt);
+            this.groupBox2.Location = new System.Drawing.Point(12, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(517, 124);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "암호화";
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(6, 15);
+            this.txtEncrypt.Multiline = true;
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.ReadOnly = true;
+            this.txtEncrypt.Size = new System.Drawing.Size(505, 103);
+            this.txtEncrypt.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(535, 40);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "암호화";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(535, 69);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "내보내기";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 288);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.msMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.msMenu;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSA User";
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem publicKeyLoadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitEToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Ch03/mook_RSAUser/mook_RSAUser/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[RSA User 디자인 및 구동 개념]
+(※RSA 서버 참조)
+
+#이벤트 핸들러
+ - publicKeyLoadToolStripMenuItem_Click(object sender, EventArgs e) : [Public Key Load] 메뉴, 공개키 로드
+ - exitEToolStripMenuItem_Click(object sender, EventArgs e) : [Exit(X)] 메뉴, 폼을 종료
+ - btnEncrypt_Click(object sender, EventArgs e) : [암호화], 공개키 데이터를 암호화
+ - btnSave_Click(object sender, EventArgs e) : [내보내기], 암호화된 데이터를 테긋트 파일 형태로 RSA Server에 전달
+*/
+namespace mook_RSAUser
+{    
+    public partial class Form1 : Form
+    {
+        string fileString = null;
+        public delegate void UpdateTextDelegate(string inputText);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //[Public Key Load] 메뉴 : RSA Server에서 전달받은 공개키를 로드하는 작업
+        private void publicKeyLoadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "Public Key Document(*.pke)|*.pke" })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(ofd.FileName, true);
+                    //파일 경로, detectEncodingFromByteOrderMarks : 파일의 시작 부분에서 바이트 순서 표시를 찾을지 여부를 나타냅니다.
+                    fileString = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+        }
+
+        //[Exit(X)] 메뉴, 폼을 종료
+        private void exitEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //[암호화] : RSA 암호화 알고리즘으로 문자열을 암호화하는 작업 수행
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if(fileString != null)
+            {
+                UpdateTextDelegate updateTextDelegate = new UpdateTextDelegate(UpdateText);
+                int bitNum = 1024;
+
+                try
+                {
+                    EncryptionThread encThread = new EncryptionThread();
+                    Thread encryptThread = new Thread(encThread.Encrypt);
+                    encryptThread.IsBackground = true;
+                    encThread.Start(new Object[] { this, updateTextDelegate, this.txtMessage.Text, bitNum, fileString });
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("에러발생 : " + ex.Message);
+                }
+            }
+        }
+
+        //델리게이트의 대리자 메서드, 암호화된 데이터를 txtEncrypt 컨트롤에 나타내는 작업
+        private void UpdateText(string inputText)
+        {
+            this.txtEncrypt.Text = inputText;
+        }
+
+        //[내보내기] : StreamWriter 클래스의 streamWriter.Write() 메서드를 이용하여 
+        //암호화된 데이터를 테긋트 파일 형태로 RSA Server에 전달
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog() { ValidateNames = true, Filter = "Text File(*.txt)|*.txt" })
+            {
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                        //매개변수 : 파일 경로, 파일을 추가하려면 true, 파일을 덮어쓰려면 false
+                        if (this.txtEncrypt.Text != null)
+                            streamWriter.Write(this.txtEncrypt.Text);
+                        streamWriter.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
