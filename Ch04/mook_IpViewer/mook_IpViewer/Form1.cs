01bd03414303b64e1134fcf91853cab814aa5d5b
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-01-19 09:19:51 +0900

    ip search

---
+++ Ch04/mook_IpViewer/mook_IpViewer/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[IP뷰어]
+ - 로컬 호스트의 이름과 IP주소를 구하는 기초적 네트워크 프로그램
+
+#이벤트 핸들러
+ - btnOk_Click(object sender, EventArgs e) : [확인], 호스트명과 IP를 구하는 작업 수행
+
+*/
+namespace mook_IpViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
