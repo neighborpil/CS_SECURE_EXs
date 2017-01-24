50d09ffd60ef1524cd1a34439324e42f35a44371
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-01-24 09:01:15 +0900

    Net Check 어플

---
+++ Ch04/mook_NetCheck/mook_NetCheck/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace mook_NetCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbAddress = new System.Windows.Forms.ListBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbTime = new System.Windows.Forms.ComboBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbAddress
+            // 
+            this.lbAddress.FormattingEnabled = true;
+            this.lbAddress.ItemHeight = 12;
+            this.lbAddress.Location = new System.Drawing.Point(12, 12);
+            this.lbAddress.Name = "lbAddress";
+            this.lbAddress.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbAddress.Size = new System.Drawing.Size(213, 316);
+            this.lbAddress.TabIndex = 0;
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.ItemHeight = 12;
+            this.lbResult.Location = new System.Drawing.Point(231, 12);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(572, 316);
+            this.lbResult.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(13, 335);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(131, 21);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(150, 335);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "입력";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // cbTime
+            // 
+            this.cbTime.FormattingEnabled = true;
+            this.cbTime.Items.AddRange(new object[] {
+            "1초",
+            "2초",
+            "3초",
+            "4초",
+            "5초"});
+            this.cbTime.Location = new System.Drawing.Point(601, 335);
+            this.cbTime.Name = "cbTime";
+            this.cbTime.Size = new System.Drawing.Size(121, 20);
+            this.cbTime.TabIndex = 3;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(728, 333);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "체크";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 366);
+            this.Controls.Add(this.cbTime);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbAddress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "포트 스캐너";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbAddress;
+        private System.Windows.Forms.ListBox lbResult;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbTime;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
