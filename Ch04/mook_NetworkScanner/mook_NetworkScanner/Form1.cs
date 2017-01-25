e5ecdbb55bccb3e9680b785821f5318fa2629ebf
neighborpil <neighborpil@naver.com>
2017-01-26 00:27:55 +0900

    Network Scanner start

---
+++ Ch04/mook_NetworkScanner/mook_NetworkScanner/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[네트워크 스캐너]
+ - 내부 네트워크에 연결된 컴퓨터를 스캔하는데 지정된 IP에 따라 검색
+*/
+namespace mook_NetworkScanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
