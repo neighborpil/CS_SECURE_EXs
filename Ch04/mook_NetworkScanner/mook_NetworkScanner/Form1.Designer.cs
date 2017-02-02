f283280ab14512e26be30d8c1d5f3df90ceb1f3c
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-02-02 10:13:07 +0900

    network scanner complete

--- Ch04/mook_NetworkScanner/mook_NetworkScanner/Form1.Designer.cs
+++ Ch04/mook_NetworkScanner/mook_NetworkScanner/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lvScan = new System.Windows.Forms.ListView();
+            this.chIp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chHostName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chMac = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pgrScan = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start IP";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(63, 9);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(146, 21);
+            this.txtStart.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "End IP";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(267, 9);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(146, 21);
+            this.txtEnd.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(419, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(104, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lvScan
+            // 
+            this.lvScan.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chIp,
+            this.chHostName,
+            this.chMac});
+            this.lvScan.GridLines = true;
+            this.lvScan.Location = new System.Drawing.Point(14, 45);
+            this.lvScan.Name = "lvScan";
+            this.lvScan.Size = new System.Drawing.Size(516, 329);
+            this.lvScan.TabIndex = 3;
+            this.lvScan.UseCompatibleStateImageBehavior = false;
+            this.lvScan.View = System.Windows.Forms.View.Details;
+            // 
+            // chIp
+            // 
+            this.chIp.Text = "IP addr";
+            this.chIp.Width = 170;
+            // 
+            // chHostName
+            // 
+            this.chHostName.Text = "HostName";
+            this.chHostName.Width = 170;
+            // 
+            // chMac
+            // 
+            this.chMac.Text = "Mac";
+            this.chMac.Width = 170;
+            // 
+            // pgrScan
+            // 
+            this.pgrScan.Location = new System.Drawing.Point(14, 380);
+            this.pgrScan.Name = "pgrScan";
+            this.pgrScan.Size = new System.Drawing.Size(516, 23);
+            this.pgrScan.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(544, 416);
+            this.Controls.Add(this.pgrScan);
+            this.Controls.Add(this.lvScan);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Network Scanner";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lvScan;
+        private System.Windows.Forms.ColumnHeader chIp;
+        private System.Windows.Forms.ColumnHeader chHostName;
+        private System.Windows.Forms.ColumnHeader chMac;
+        private System.Windows.Forms.ProgressBar pgrScan;
     }
 }
 
--- Ch04/mook_NetworkScanner/mook_NetworkScanner/Form1.cs
+++ Ch04/mook_NetworkScanner/mook_NetworkScanner/Form1.cs
@@ -4,20 +4,168 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 /*
 [네트워크 스캐너]
  - 내부 네트워크에 연결된 컴퓨터를 스캔하는데 지정된 IP에 따라 검색
+ - 내부 공간이 분리되어 있고 사용자 컴퓨터에 정확히 IP가 부여되어 사용되지 않는 환경에서 어떤 IP가 사용되는지 쉽게 검색 가능
+ - 내부 네트워크에 연결된 컴퓨터를 검색하기 위하여 iphlpapi.dll 어셈블리를 이용
+
+#이벤트 핸들러
+ - btnSearch_Click(object sender, EventArgs e) : [검색], 네트워크에 연결된 컴퓨터를 찾는 작업을 수행
+ - Form1_FormClosing(object sender, FormClosingEventArgs e) : 스레드 종료
 */
 namespace mook_NetworkScanner
 {
     public partial class Form1 : Form
     {
+        Thread NetworkScan = null;
+
+        //SendARP() ; LAN에서 네트워크 카드에 ARP 메시지를 보내기 위한 메서드
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        static extern int SendARP(int DestIP, int SrcIP, byte[] pMacAddr, ref uint PhyAddrLen);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //[검색] : 네트워크를 검색하는 메서드 실행
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            NetworkScan = new Thread(NetworkCheck);
+            NetworkScan.Start();
+        }
+
+        //지정된 IP 대역에 연결된 컴퓨터를 검색하여 lvScan 컨트롤에 나타내는 작업
+        private void NetworkCheck()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            try
+            {
+                this.lvScan.Items.Clear();
+
+                Application.DoEvents();
+
+                IPAddress ip = IPAddress.Parse(this.txtStart.Text);
+                IPAddress ip2 = IPAddress.Parse(this.txtEnd.Text);
+                IPAddress ipScan = null;
+                string hostName = null;
+
+                int[] aa = new int[4], bb = new int[4];
+
+                aa[0] = Convert.ToInt32(ip.GetAddressBytes()[0]);
+                aa[1] = Convert.ToInt32(ip.GetAddressBytes()[1]);
+                aa[2] = Convert.ToInt32(ip.GetAddressBytes()[2]);
+                aa[3] = Convert.ToInt32(ip.GetAddressBytes()[3]);
+
+                bb[0] = Convert.ToInt32(ip2.GetAddressBytes()[0]);
+                bb[1] = Convert.ToInt32(ip2.GetAddressBytes()[1]);
+                bb[2] = Convert.ToInt32(ip2.GetAddressBytes()[2]);
+                bb[3] = Convert.ToInt32(ip2.GetAddressBytes()[3]);
+
+                this.pgrScan.Minimum = Convert.ToInt32(ip.GetAddressBytes()[3]);
+                this.pgrScan.Maximum = Convert.ToInt32(ip2.GetAddressBytes()[3]);
+                this.pgrScan.Value = Convert.ToInt32(ip.GetAddressBytes()[3]);
+
+                for (int a = aa[0]; a <= bb[0]; a++)
+                {
+                    for (int b = aa[1]; b <= bb[1]; b++)
+                    {
+                        for (int c = aa[2]; c <= bb[2]; c++)
+                        {
+                            for (int d = aa[3]; d <= bb[3]; d++)
+                            {
+                                this.pgrScan.Value = d;
+                                ipScan = IPAddress.Parse(a.ToString() + "." + b.ToString() + "." + c.ToString() + "." + d.ToString());
+                                Ping pingSender = new Ping();
+                                PingOptions options = new PingOptions();
+                                options.DontFragment = true;
+                                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                                int timeout = 150;
+                                PingReply reply = pingSender.Send(ipScan, timeout, buffer, options);
+                                hostName = null;
+                                string macAddr = null;
+                                if(reply.Status == IPStatus.Success)
+                                {
+                                    hostName = GetHostName(ipScan);
+                                    macAddr = GetMacUsingARP(ipScan.ToString());
+                                    ListViewItem itm = new ListViewItem();
+                                    itm.Text = ipScan.ToString();
+                                    itm.SubItems.Add(hostName);
+                                    itm.SubItems.Add(macAddr);
+                                    this.lvScan.Items.Add(itm);
+                                }
+
+                                if (d == this.pgrScan.Maximum)
+                                    MessageBox.Show("네트워크 스캔을 완료하였습니다", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                NetworkScan.Abort();
+            }
+        }
+
+        //파리미터로 IP 주소를 전달받아 호스트 이름을 반환
+        private string GetHostName(IPAddress pAddress)
+        {
+            try
+            {
+                IPHostEntry entry = Dns.GetHostEntry(pAddress); //호스트의 주소정보를 포함하는 IPHostEntry 개체 반환
+                #region IPHostEntry 속성
+                /*
+                #IPHostEntry 속성
+                 - AddressList : 호스트와 연결된 IP 주소 목록을 가져오거나 설정
+                 - Aliases : 호스트와 연결된 별칭 목록을 가져오거나 반환
+                 - HostName : 호스트의 DNS 이름을 가져오거나 반환
+                */
+                #endregion
+                return entry.HostName; //호스트의 이름 반환
+            }
+            catch (Exception)
+            {
+                return pAddress.ToString();
+            }
+        }
+        
+        //파라미터로 IP를 전달받아 해당하는 시스템의 MAC 주소반환
+        private string GetMacUsingARP(string IPAddr)
+        {
+            IPAddress IP = IPAddress.Parse(IPAddr);
+            byte[] macAddr = new byte[6];
+            uint macAddrLen = (uint)macAddr.Length;
+            if (SendARP((int)IP.Address, 0, macAddr, ref macAddrLen) != 0) //iphlpapi.dll파일에 정의된 SendARP()메서드 호출하여 
+                return "ARP command failed";                               //반환값이 0이면 'ARP'가 성공한 것이고 그외의 반환값은 실패
+                                                                //바이트 배열 타입의 매개변수인 macAddr에 해당 시스템의 MAC주소가
+                                                                //참조타칩 매개변수(ref)인 macAddrLen에 바이트 배열의 길이가 저장
+            string[] str = new string[(int)macAddrLen];
+            for (int i = 0; i < macAddrLen; i++)
+                str[i] = macAddr[i].ToString("x2"); //16진수의 두자리로 저장
+            return string.Join(":", str); //string배열의 각요소 사이에 ":" 삽입
+        }
+
+        //스레드 종료
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (NetworkScan != null)
+                NetworkScan.Abort();
         }
     }
 }
