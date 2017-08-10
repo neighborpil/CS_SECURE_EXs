b0d770e5bcc4ce73fc3f0b3edb61dcecc9e4b971
neighborpil <neighborpil@naver.com>
2017-08-11 02:19:38 +0900

    ui making

--- Ch04/mook_PacketSniffer/mook_PacketSniffer/Form1.Designer.cs
+++ Ch04/mook_PacketSniffer/mook_PacketSniffer/Form1.Designer.cs
@@ -31,24 +31,51 @@
             this.tsMenu = new System.Windows.Forms.ToolStrip();
             this.lvReceivedPackets = new System.Windows.Forms.ListView();
             this.tvPacketDetail = new System.Windows.Forms.TreeView();
+            this.tslblIp = new System.Windows.Forms.ToolStripLabel();
+            this.tscbIp = new System.Windows.Forms.ToolStripComboBox();
+            this.tslblNum = new System.Windows.Forms.ToolStripLabel();
+            this.tscbNum = new System.Windows.Forms.ToolStripComboBox();
+            this.chNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSource = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDest = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chProtocol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPack = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tsbtnStar = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnStop = new System.Windows.Forms.ToolStripButton();
+            this.tsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // tsMenu
             // 
+            this.tsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnStar,
+            this.tsbtnStop,
+            this.tslblIp,
+            this.tscbIp,
+            this.tslblNum,
+            this.tscbNum});
             this.tsMenu.Location = new System.Drawing.Point(0, 0);
             this.tsMenu.Name = "tsMenu";
-            this.tsMenu.Size = new System.Drawing.Size(989, 25);
+            this.tsMenu.Size = new System.Drawing.Size(755, 25);
             this.tsMenu.TabIndex = 0;
             this.tsMenu.Text = "toolStrip1";
             // 
             // lvReceivedPackets
             // 
+            this.lvReceivedPackets.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chNo,
+            this.chTime,
+            this.chSource,
+            this.chDest,
+            this.chProtocol,
+            this.chPack});
             this.lvReceivedPackets.Dock = System.Windows.Forms.DockStyle.Top;
             this.lvReceivedPackets.FullRowSelect = true;
             this.lvReceivedPackets.GridLines = true;
             this.lvReceivedPackets.Location = new System.Drawing.Point(0, 25);
             this.lvReceivedPackets.Name = "lvReceivedPackets";
-            this.lvReceivedPackets.Size = new System.Drawing.Size(989, 97);
+            this.lvReceivedPackets.Size = new System.Drawing.Size(755, 97);
             this.lvReceivedPackets.TabIndex = 1;
             this.lvReceivedPackets.UseCompatibleStateImageBehavior = false;
             this.lvReceivedPackets.View = System.Windows.Forms.View.Details;
@@ -58,19 +85,99 @@
             this.tvPacketDetail.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tvPacketDetail.Location = new System.Drawing.Point(0, 122);
             this.tvPacketDetail.Name = "tvPacketDetail";
-            this.tvPacketDetail.Size = new System.Drawing.Size(989, 464);
+            this.tvPacketDetail.Size = new System.Drawing.Size(755, 464);
             this.tvPacketDetail.TabIndex = 2;
+            // 
+            // tslblIp
+            // 
+            this.tslblIp.Name = "tslblIp";
+            this.tslblIp.Size = new System.Drawing.Size(83, 22);
+            this.tslblIp.Text = "Select IP Addr";
+            // 
+            // tscbIp
+            // 
+            this.tscbIp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tscbIp.Name = "tscbIp";
+            this.tscbIp.Size = new System.Drawing.Size(121, 25);
+            // 
+            // tslblNum
+            // 
+            this.tslblNum.Name = "tslblNum";
+            this.tslblNum.Size = new System.Drawing.Size(79, 22);
+            this.tslblNum.Text = "aPacket Num";
+            // 
+            // tscbNum
+            // 
+            this.tscbNum.Items.AddRange(new object[] {
+            "100",
+            "300",
+            "400",
+            "500",
+            "1000"});
+            this.tscbNum.Name = "tscbNum";
+            this.tscbNum.Size = new System.Drawing.Size(121, 25);
+            // 
+            // chNo
+            // 
+            this.chNo.Text = "No.";
+            this.chNo.Width = 70;
+            // 
+            // chTime
+            // 
+            this.chTime.Text = "Time";
+            this.chTime.Width = 180;
+            // 
+            // chSource
+            // 
+            this.chSource.Text = "Source";
+            this.chSource.Width = 150;
+            // 
+            // chDest
+            // 
+            this.chDest.Text = "Destination";
+            this.chDest.Width = 150;
+            // 
+            // chProtocol
+            // 
+            this.chProtocol.Text = "Protocol";
+            this.chProtocol.Width = 100;
+            // 
+            // chPack
+            // 
+            this.chPack.Text = "Package Size";
+            this.chPack.Width = 100;
+            // 
+            // tsbtnStar
+            // 
+            this.tsbtnStar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnStar.Image = global::mook_PacketSniffer.Properties.Resources.play_on;
+            this.tsbtnStar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnStar.Name = "tsbtnStar";
+            this.tsbtnStar.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnStar.Text = "Start";
+            // 
+            // tsbtnStop
+            // 
+            this.tsbtnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnStop.Enabled = false;
+            this.tsbtnStop.Image = global::mook_PacketSniffer.Properties.Resources.stop;
+            this.tsbtnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnStop.Name = "tsbtnStop";
+            this.tsbtnStop.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnStop.Text = "Stop";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 586);
+            this.ClientSize = new System.Drawing.Size(755, 586);
             this.Controls.Add(this.tvPacketDetail);
             this.Controls.Add(this.lvReceivedPackets);
             this.Controls.Add(this.tsMenu);
             this.Name = "Form1";
             this.Text = "PacketSniffer";
+            this.tsMenu.ResumeLayout(false);
+            this.tsMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +188,18 @@
         private System.Windows.Forms.ToolStrip tsMenu;
         private System.Windows.Forms.ListView lvReceivedPackets;
         private System.Windows.Forms.TreeView tvPacketDetail;
+        private System.Windows.Forms.ToolStripButton tsbtnStar;
+        private System.Windows.Forms.ToolStripButton tsbtnStop;
+        private System.Windows.Forms.ToolStripLabel tslblIp;
+        private System.Windows.Forms.ToolStripComboBox tscbIp;
+        private System.Windows.Forms.ToolStripLabel tslblNum;
+        private System.Windows.Forms.ToolStripComboBox tscbNum;
+        private System.Windows.Forms.ColumnHeader chNo;
+        private System.Windows.Forms.ColumnHeader chTime;
+        private System.Windows.Forms.ColumnHeader chSource;
+        private System.Windows.Forms.ColumnHeader chDest;
+        private System.Windows.Forms.ColumnHeader chProtocol;
+        private System.Windows.Forms.ColumnHeader chPack;
     }
 }
 
