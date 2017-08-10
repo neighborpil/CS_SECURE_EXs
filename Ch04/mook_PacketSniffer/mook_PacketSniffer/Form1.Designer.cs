17719087c0e426af54569313c1c38beb1a4e3f74
neighborpil <neighborpil@naver.com>
2017-08-11 01:02:11 +0900

    restart ch4 packetsniffing

---
+++ Ch04/mook_PacketSniffer/mook_PacketSniffer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace mook_PacketSniffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tsMenu = new System.Windows.Forms.ToolStrip();
+            this.lvReceivedPackets = new System.Windows.Forms.ListView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tsMenu
+            // 
+            this.tsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tsMenu.Name = "tsMenu";
+            this.tsMenu.Size = new System.Drawing.Size(1040, 25);
+            this.tsMenu.TabIndex = 0;
+            this.tsMenu.Text = "toolStrip1";
+            // 
+            // lvReceivedPackets
+            // 
+            this.lvReceivedPackets.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lvReceivedPackets.FullRowSelect = true;
+            this.lvReceivedPackets.Location = new System.Drawing.Point(0, 25);
+            this.lvReceivedPackets.Name = "lvReceivedPackets";
+            this.lvReceivedPackets.Size = new System.Drawing.Size(1040, 97);
+            this.lvReceivedPackets.TabIndex = 1;
+            this.lvReceivedPackets.UseCompatibleStateImageBehavior = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 122);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(1040, 407);
+            this.treeView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 529);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.lvReceivedPackets);
+            this.Controls.Add(this.tsMenu);
+            this.Name = "Form1";
+            this.Text = "PacketSniffer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tsMenu;
+        private System.Windows.Forms.ListView lvReceivedPackets;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Ch04/mook_PacketSniffer/mook_PacketSniffer/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+[네트워크 패킷 스니핑]
+ - 네트워크 패킷을 분석하기 위한 분석 도구
+ - 내부 네트워크의 패킷을 모니터링 하기 위하여도 사용
+ - 내부에서 지나다니는 패킷을 한 컴퓨터에 모이게 한 다음, 네트워크 패킷 스티핑 프로그램으로 패킷을 분석하면 그 안에 비밀번호등 중요 정보 획득
+ - 해커들은 해킹을 위한 준비작업으로 네트워크 패킷을 분석하는 작업을 거치는데 이러한 패킷 스니핑 프로그램을 사용
+
+#예제의 특징
+ - 패킷을 상세히 분석하지는 않음
+ - IP, TCP, UDP 등 프로토콜을 구분하여 다양한 정보를 나타내 준다
+ - 패킷을 상세히 분석하기 위해서는 TCP/IP, UDP 프로토콜에 대한 사전 지식 습득이 필요
+*/
+namespace mook_PacketSniffer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
